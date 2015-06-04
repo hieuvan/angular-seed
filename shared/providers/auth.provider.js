5a6ded57900deccc89b2c61646c65e0be978ba75
subash <subash.adhikari@acer.edu.au>
2015-06-04 15:48:20 +1000

    add doc block

--- shared/providers/auth.provider.js
+++ shared/providers/auth.provider.js
@@ -10,8 +10,16 @@ define(function(require) {
 
     this.$get = ['$cookieStore', 'HttpService', '$q', function($cookies, HttpService, $q) {
 
-      var user, tokenName = '_token';
+      var tokenName = '_token';
 
+      /**
+       * Perform http request to the login url and get token
+       * if token is recieved its considered that login was successful
+       * and token is set to cookie
+       *
+       * @param formdata User credentials
+       * @return User Model
+       */
       var login = function(formdata) {
 
         return HttpService.post(self.loginUrl, formdata)
@@ -24,13 +32,22 @@ define(function(require) {
           });
       };
 
+      /**
+       * Logout the current logged in user
+       */
       var logout = function() {
         removeToken();
-        user = null;
       };
 
+      /**
+       * Get the user associated with the current token
+       *
+       * @return false || UserModel
+       */
       var getUser = function() {
         var payload = getPayload();
+
+        if (!payload) return false;
 
         return new User({
           id: payload.id,
@@ -38,6 +55,12 @@ define(function(require) {
         });
       };
 
+      /**
+       * Check if the user is logged in
+       * Also checks if the token is expired
+       *
+       * @return boolean
+       */
       var isAuthenticated = function() {
         if (!getToken()) return false;
 
@@ -48,24 +71,54 @@ define(function(require) {
         return isTokenExpired(payload.exp);
       };
 
+      /**
+       * Check if token is expired
+       * if expired also delete it
+       *
+       * @return boolean
+       */
       var isTokenExpired = function(exp) {
         if (!exp) return true; // no expiry is set in the token
 
-        return Math.round(new Date().getTime() / 1000) <= exp;
+        if (Math.round(new Date().getTime() / 1000) <= exp) return true;
+
+        // if token expired, remove it
+        removeToken();
+        return false;
       };
 
+      /**
+       * Set given token to cookie store
+       *
+       * @param string token Token to store
+       */
       var setToken = function(token) {
         $cookies.put(tokenName, token);
       };
 
+      /**
+       * Remove if any token exists
+       */
       var removeToken = function() {
         $cookies.remove(tokenName);
       };
 
+      /**
+       * Get the current token
+       *
+       * @return string || null
+       */
       var getToken = function() {
         return $cookies.get(tokenName);
       };
 
+      /**
+       * Decode the token and get the Json object associated to it
+       * If not token provided will get the token from cookie
+       *
+       * @param string token Optional.
+       * @return string || boolean Token or false
+       */
       var getPayload = function(token) {
         token = token || getToken();
 
@@ -79,6 +132,7 @@ define(function(require) {
         return false;
       };
 
+      // public methods
       return {
         getUser: getUser,
         login: login,
