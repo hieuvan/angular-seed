bad3046ebb1c962fd7cc6a2ae488ba2d34090903
Subash Adhikari <subash.adhikari@acer.edu.au>
2015-09-11 11:26:31 +1000

    add docblock

--- app/shared/libs/object/object-collection.js
+++ app/shared/libs/object/object-collection.js
@@ -30,29 +30,52 @@ define(function(require) {
 
   prototype = ObjectCollection.prototype;
 
-  prototype.add = function(models) {
-    var self = this;
-    models = _.isArray(models) ? models : [models];
-
-    _.each(models, function(model) {
-      self._collection.push(model);
-    });
-  };
-
+  /**
+   * Get all items in collection
+   *
+   * @return {array}
+   */
   prototype.getAll = function() {
     return this._collection;
   };
 
-  prototype.flatten = function() {
-    var flattened = [];
+  /**
+   * Find items in collection
+   *
+   * @param {array} ids
+   * @return {this}
+   */
+  prototype.findByIds = function(ids) {
+    ids = ids || [];
 
-    _.each(this.getAll(), function(model) {
-      flattened.push(model.get());
-    });
+    this._result = _.chain(this._collection)
+      .filter(function(model) {
+        return _.contains(ids, model.get('id'));
+      })
+      .compact()
+      .value();
 
-    return flattened;
+    return this;
   };
 
+  /**
+   * Find given item by id
+   *
+   * @param id
+   * @return {object}
+   */
+  prototype.findById = function(id) {
+    return _.find(this._collection, function(candidate) {
+      return candidate.get('id') === id;
+    });
+  };
+
+  /**
+   * Pluck items from collection
+   *
+   * @param {array|string} properties
+   * @return {array}
+   */
   prototype.pluck = function(properties) {
     var self = this,
         properties = _.isArray(properties) ? properties : [properties];
@@ -68,6 +91,27 @@ define(function(require) {
     });
   };
 
+  /**
+   * Add model in collection
+   *
+   * @param {array|object} models
+   * @return {undefined}
+   */
+  prototype.add = function(models) {
+    var self = this;
+    models = _.isArray(models) ? models : [models];
+
+    _.each(models, function(model) {
+      self._collection.push(model);
+    });
+  };
+
+  /**
+   * Remove element by id
+   *
+   * @param id
+   * @return {this}
+   */
   prototype.removeById = function(id) {
     this._collection = _.filter(this._collection, function(model) {
       return model.get('id') != id
@@ -77,20 +121,11 @@ define(function(require) {
   };
 
   /**
-   * check if the collection is empty
-   *
-   * @return {undefined}
-   */
-  prototype.isEmpty = function() {
-    return !this._collection.length > 0;
-  };
-
-  /**
    * Substract the items of the given collection from the collection
    *
    * @param collection Collection to intersect with
    *
-   * @return ObjectCollection
+   * @return {ObjectCollection}
    */
   prototype.substract = function(collection, key) {
     if (_.isUndefined(key)) key = 'id';
@@ -109,68 +144,11 @@ define(function(require) {
   };
 
   /**
-   * @TODO: implement this function
+   * Do something recursively to each items
    *
-   * collection.sortby('property').getAll();
+   * @param callback
+   * @return {undefined}
    */
-  prototype.sortby = function(property) {
-    // this._collection = logic that returns sorted collection
-    return this;
-  };
-
-  prototype.result = function() {
-    return this._result;
-  };
-
-  prototype.findByIds = function(ids) {
-    ids = ids || [];
-
-    this._result = _.chain(this._collection)
-      .filter(function(model) {
-        return _.contains(ids, model.get('id'));
-      })
-      .compact()
-      .value();
-
-    return this;
-  };
-
-  prototype.andUpdate = function(key, value) {
-    return this.invoke('update', key, value);
-  };
-
-  prototype.AndSet = function(key, value) {
-    return this.invoke('set', key, value);
-  };
-
-  prototype.Andadd = function(key, value) {
-    return this.invoke('add', key, value);
-  };
-
-  prototype.andRemove = function() {
-    return this.invoke('remove', key, value);
-  };
-
-  prototype.invoke = function(fn) {
-    if (!validFunction(this._result[0], fn)) {
-      throw new Error(fn + ' is not a valid function available in ' + this._result[0].toString() +'.');
-    }
-
-    var result = _.invoke(this._result, fn, arguments[1], arguments[2]);
-
-    return this;
-  };
-
-  prototype.findById = function(id) {
-    return _.find(this._collection, function(candidate) {
-      return candidate.get('id') === id;
-    });
-  };
-
-  prototype.toString = function() {
-    return this._className;
-  };
-
   prototype.each = function(callback) {
     if (!_.isFunction(callback)) {
       throw new Error(
@@ -185,10 +163,140 @@ define(function(require) {
     });
   };
 
-  var validateData = function(data) {
+  /**
+   * Get flat array representation of collection
+   *
+   * @return {array}
+   */
+  prototype.flatten = function() {
+    var flattened = [];
 
+    _.each(this.getAll(), function(model) {
+      flattened.push(model.get());
+    });
+
+    return flattened;
   };
 
+
+  /**
+   * @TODO: implement this function
+   *
+   * collection.sortby('property').getAll();
+   */
+  prototype.sortby = function(property) {
+    // this._collection = logic that returns sorted collection
+    return this;
+  };
+
+  /**
+   * Update all items for given key with value
+   *
+   * @param key
+   * @param value
+   * @return {this}
+   */
+  prototype.andUpdate = function(key, value) {
+    return this.invoke('update', key, value);
+  };
+
+  /**
+   * Set all items key value
+   *
+   * @param key
+   * @param value
+   * @return {this}
+   */
+  prototype.andSet = function(key, value) {
+    return this.invoke('set', key, value);
+  };
+
+  /**
+   * Add item in the model for given key with value
+   *
+   * @param key
+   * @param value
+   * @return {undefined}
+   */
+  prototype.andAdd = function(key, value) {
+    return this.invoke('add', key, value);
+  };
+
+  prototype.andRemove = function() {
+    return this.invoke('remove', key, value);
+  };
+
+  /**
+   * Invoke given function in all items in collection
+   *
+   * @param fn
+   * @return {this}
+   */
+  prototype.invoke = function(fn) {
+    if (!validFunction(this._result[0], fn)) {
+      throw new Error(fn + ' is not a valid function available in ' + this._result[0].toString() +'.');
+    }
+
+    var result = _.invoke(this._result, fn, arguments[1], arguments[2]);
+
+    return this;
+  };
+
+  /**
+   * Get the result of query performed in the collection
+   * Returns result of search, find
+   *
+   * @return {array}
+   */
+  prototype.result = function() {
+    return this._result;
+  };
+
+  /**
+   * check if the collection is empty
+   *
+   * @return {boolean}
+   */
+  prototype.isEmpty = function() {
+    return !this.count() > 0;
+  };
+
+  /**
+   * Get size of collection
+   *
+   * @return {int}
+   */
+  prototype.count = function() {
+    return this._collection.length;
+  };
+
+  /**
+   * Get class name
+   *
+   * @return {string}
+   */
+  prototype.toString = function() {
+    return this._className;
+  };
+
+  /**
+   * Validate data
+   *
+   * @param data
+   * @return {undefined}
+   */
+  var validateData = function(data) {
+    // STUB
+  };
+
+  /**
+   * Check if invoked function is valid
+   *
+   * @param candidate
+   * @param fn
+   * @throws Error
+   * @return {boolean}
+   */
   var validFunction = function(candidate, fn) {
     var valid;
 
