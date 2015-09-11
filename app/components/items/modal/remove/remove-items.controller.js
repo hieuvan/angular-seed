648af7b32047ebf0f8b1d8e76726e3efb2dd97dc
Subash Adhikari <subash.adhikari@acer.edu.au>
2015-09-11 17:18:32 +1000

    remove ajax update from remove controller

--- app/components/items/modal/remove/remove-items.controller.js
+++ app/components/items/modal/remove/remove-items.controller.js
@@ -1,63 +1,24 @@
 'use strict';
 
 define(function() {
-  return ['node', '$modalInstance', '$stateParams', 'ItemService', 'ngToast',
-    function(node, $modalInstance, $stateParams, ItemService, ngToast) {
+  return ['node', '$modalInstance', 'ngToast',
+    function(node, $modalInstance, ngToast) {
 
     var vm = this,
         currentNode = node.$modelValue;
 
     vm.title = currentNode.uid;
 
-    vm.multipleItems = !_.isEmpty(currentNode.items);
-
     vm.removeItems = function() {
-      // @TODO: update this to use the function in the directive
-      // at the moment there is none, but add a new one
-      var items = pluckItems(currentNode, 'id');
+      node.remove();
+
+      ngToast.danger('Item removed from tree.')
 
       vm.cancel();
-
-      ItemService.removeFormItem(items, $stateParams.title)
-        .then(function(items) {
-          node.remove();
-
-          ngToast.danger('Item removed from tree.')
-        });
-
     };
 
     vm.cancel = function () {
       $modalInstance.dismiss('cancel');
     };
-
-    /**
-     * Pluck deeply nested keys value from node.items
-     *
-     * @param node Haystack
-     * @param string key Object key to pluck
-     *
-     * @returns array
-     */
-    var pluckItems = function(node, key) {
-      var result = [];
-
-      result.push(node[key]);
-
-      if (_.isUndefined(node.items)) return result;
-
-      _.each(node.items, function(item) {
-        result.push(item[key]);
-
-        var children = pluckItems(item, key);
-
-        if (!_.isEmpty(children)) {
-          result = _.union(result, children);
-        }
-      });
-
-      return result;
-    };
-
   }];
 });
