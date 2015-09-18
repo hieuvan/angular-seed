725bc838fbcf6225d957b90a5d8353275b11797a
Subash Adhikari <subash.adhikari@acer.edu.au>
2015-09-18 11:52:34 +1000

    add application config

--- app/app.main.js
+++ app/app.main.js
@@ -1,10 +1,17 @@
 'use strict';
 
-require([
-  'app.module',
-  'underscore'
-], function() {
+define(function(require) {
+  require('underscore');
+
+  var angular = require('angular'),
+      app = require('app.module'),
+      config = require('shared/config/config');
+
   var $html = angular.element(document.getElementsByTagName('html'));
+
+  config.defaultUrl = window.location.hash.substr(1);
+
+  app.constant('config', config);
 
   angular.element().ready(function() {
     angular.bootstrap($html, ['app']);
